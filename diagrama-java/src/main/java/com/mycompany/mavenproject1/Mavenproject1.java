a6b5252bb8eb5f51dca790a539146710473bd7ca
davydschumacher <dzinschumacher@gmail.com>
2024-12-12 19:50:06 -0300

    Add files via upload

---
+++ diagrama-java/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,81 @@
+public class TurmaFrame{
+    private JButton listarAlunosButton;
+    private JButton resultadosButton;
+    private JButton novoAlunoButton;
+    private Controlador controlador;
+    
+        public static void main (string[]: args){
+        }
+        private void thisWindowClosing)(){
+        }
+        private void novoAlunoButtonActionPerformed(){
+        }
+        private void listarAlunosButtonActionPerformed(){
+        }
+        private void resultadosButtonActionPerformed(){
+        }
+        private void resultadosButtonActionPermed(){
+    }
+}
+
+private class Controlador{
+
+    private Aluno[] alunos;
+    private int pos;
+    private JButton alunosCadastradosFrame;
+    
+        public Aluno[] Alunos(){
+        }
+        public void cadastrarAluno(){
+        }
+        public void listarAlunos(){
+        }
+        public void terminarEdicao(){
+        }
+        public boolean salvarAlunos(){
+        }
+        public void editarAlunos(){
+        }
+        public void terminarEdicao(){
+        }
+        public double calcularMedia(){
+        }
+}
+
+public class AlunoFrame{
+    
+    private JTextField nomeTextField;
+    private JButton okButton;
+    private JButton cancelarJButton;
+    private JTextField notaTextField;
+    private Aluno aluno;
+    
+        public void setControlador(){
+        }
+        private void thisWindowClosing(){
+        }
+        private void  cancelarButtonActionPerformed(){
+        }
+        private void cancelar(){
+        }
+        private void okBurronActionPerformed(){
+        }
+        public void carregarDados(){
+        }
+}
+
+public class AlunosCadastradosFrame{
+    private JMenuItem cadastrarMenuItem;
+    private JMenuItem editarMenuItem;
+    private JList alunosList;
+    private Controlador controlador;
+    
+        public void carregarDados(){       
+        }
+        public void cadastrarMenuItemActionPerformed(){        
+        }
+        private void editarMenuItemActionPerformed(){       
+        }
+        public void atualizar(){
+        }
+}
